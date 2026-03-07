0a475b58ee8fc28b79691702ec1601f2f40d48b1
Maxim Rysevets <root@effcore.com>
2026-03-07 23:57:54 +0300

    fix

--- app/main app/ExtensionStatus.swift
+++ app/main app/ExtensionStatus.swift
@@ -8,10 +8,11 @@ import FinderSync
 
 struct ExtensionStatus: View {
 
+    @Environment(\.colorScheme) private var colorScheme
     @State private var isEnabled: Bool = false
 
     public var body: some View {
-        VStack(spacing: 10) {
+        VStack(spacing: 15) {
 
             Image(systemName: self.isEnabled ? "checkmark.circle.fill" : "xmark.circle.fill")
                 .resizable()
@@ -24,16 +25,47 @@ struct ExtensionStatus: View {
                 .background(Color.white)
                 .clipShape(Circle())
 
+            Text(
+                self.isEnabled ?
+                    NSLocalizedString("extension is enabled" , comment: "") :
+                    NSLocalizedString("extension is disabled", comment: "")
+            ).font(.system(size: 16, weight: .bold))
+
+            ButtonCustom(
+                NSLocalizedString("Open System Preference", comment: ""),
+                colorStyle: .custom(text: nil, background: nil),
+                isFlat: false,
+                flexibility: .none
+            ) { FinderSync.FIFinderSyncController.showExtensionManagementInterface() }
+
         }
-        .onAppear {
-            self.isEnabled = FIFinderSyncController.isExtensionEnabled
-        }
+        .padding(20)
+        .frame(maxWidth: .infinity)
+        .background(self.GroupBackground())
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+        .padding(20)
+        .onAppear              { self.isEnabled = FIFinderSyncController.isExtensionEnabled }
+        .onAppBecomeForeground { self.isEnabled = FIFinderSyncController.isExtensionEnabled }
+    }
+
+    @ViewBuilder func GroupBackground() -> some View {
+        RoundedRectangle(cornerRadius: 15)
+            .stroke(
+                self.colorScheme == .dark ?
+                    Color.white.opacity(0.5) :
+                    Color.black.opacity(0.5),
+                lineWidth: 1
+            )
+            .background(
+                self.colorScheme == .dark ?
+                    Color.black.opacity(0.2) :
+                    Color.white.opacity(0.7)
+            )
     }
 
 }
 
 #Preview {
     ExtensionStatus()
-        .padding(20)
-        .frame(width: 300)
+        .frame(width: 400)
 }
