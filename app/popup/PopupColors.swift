ec6456a5a5d4808fb6aa07e4526b9640cfc6e8ea
Maxim Rysevets <root@effcore.com>
2026-03-09 17:40:16 +0300

    fix

--- app/popup/PopupColors.swift
+++ app/popup/PopupColors.swift
@@ -8,10 +8,9 @@ import SwiftUI
 extension Color {
 
     struct PopupCustomColorSet {
-        public let head     = Color("color Popup Head Background")
-        public let gridTint = Color("color Popup Head Row Tint")
-        public let body     = Color("color Popup Body Background")
-        public let foot     = Color("color Popup Foot Background")
+        public let head = Color("color Popup Head Background")
+        public let body = Color("color Popup Body Background")
+        public let foot = Color("color Popup Foot Background")
     }
 
     static let popup = PopupCustomColorSet()
